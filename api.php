bef4ce7b70ed7590bffe3dd8f93671ff30724be8
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-01-28 21:46:32 +0100

    Songs endpoint for testing

--- api.php
+++ api.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: *');  
 require 'Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
@@ -9,21 +10,44 @@ $app = new \Slim\Slim(array(
 	'debug' => true,			// true/false
 ));
 
+$app->contentType("application/json");
+
 $app->setName('DJsMusic');
 
 
 // API endpoints
 $app->get('/', 'getInfo');
+$app->get('/songs/:param', 'songs');
 
 $app->run();
 
 // Functions
 function getInfo(){
-	echo 'DJs Music API v1.0';
+	echo json_encode('DJs Music API v1.0');
+}
+
+function songs($param){
+	switch($param){
+		case 'latest':
+			$songs = Array(
+				Array('title'=> 'Latest Song 1', 'id'=>1, 'artist'=> 'DJ Name 1', 'thumb'=> 'img/logo.jpg', 'duration'=> 124, 'downloads' => 34, 'rating' => 3),
+				Array('title'=> 'Latest Song 2', 'id'=>2, 'artist'=> 'DJ Name 2', 'thumb'=> 'img/logo.jpg', 'duration'=> 324, 'downloads' => 74,'rating' => 5)
+			);
+			break;
+		case 'top':
+			$songs = Array(
+				Array('title'=> 'Top Song 1', 'id'=>1, 'artist'=> 'DJ Name 1', 'thumb'=> 'img/logo.jpg', 'duration'=> 124, 'downloads' => 34, 'rating' => 3),
+				Array('title'=> 'Top Song 2', 'id'=>2, 'artist'=> 'DJ Name 2', 'thumb'=> 'img/logo.jpg', 'duration'=> 324, 'downloads' => 74,'rating' => 5)
+			);
+			break;
+	}
+	
+	
+	echo json_encode($songs);
 }
 
 function getConnection() {
-    $dbhost="127.0.0.1";
+    $dbhost="localhost";
     $dbuser="root";
     $dbpass="";
     $dbname="directory";
