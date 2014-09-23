a9eb2814ae586259dec17764ced30b89a4dfa5fa
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-09-23 18:02:21 +0200

    Added salting

--- keys.sample.php
+++ keys.sample.php
@@ -7,3 +7,6 @@ define('DB_USER','user');
 define('DB_HOST','localhost');
 define('DB_PASS','pass');
 define('DB_NAME','db');
+
+define('DB_SALT_PRE','hashing salt pre');
+define('DB_SALT_POST','hashing salt post');
